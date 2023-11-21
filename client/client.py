dafebde82e6199e1e5420a67faa5d9b3fa35b6ba
La Minh Duc <laminhduc200@gmail.com>
2023-11-21 20:24:11 +0700

    test server

--- client/client.py
+++ client/client.py
@@ -1,7 +1,15 @@
 import socket
 
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 5500
+ADDR = (IP, PORT)
+SIZE = 1024
+ENCODING = "utf-8"
+
 def main():
-    print("hello from client")
+    client_welcom = ADDR
+    print(client_welcom)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 if __name__ == "__main__":
     main()
--- server/server.py
+++ server/server.py
@@ -2,12 +2,21 @@ import socket
 import os
 import threading
 
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 5500
+ADDR = (IP, PORT)
+SIZE = 1024
 ENCODING = "utf-8"
 
 def main():
-    raw_data = "The server is online".encode(ENCODING)
-    print(raw_data)
-    print(raw_data.decode(ENCODING))
+    print("Server is online")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen()
+    print("Server is listening")
+    while True:
+        connect, addr = server.accept()
+        print(f"New connection: {addr}")
 
 if __name__ == "__main__":
     main()
