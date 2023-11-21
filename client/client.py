6d78252c5f0736d495b3945a1a39615ca49e237c
La Minh Duc <laminhduc200@gmail.com>
2023-11-22 02:50:12 +0700

    update client.py

--- client/client.py
+++ client/client.py
@@ -7,6 +7,7 @@ PORT = 5500
 ADDR = (IP_ADDR, PORT)
 SIZE = 1024
 ENCODING = "utf-8"
+DATA_PATH = "data/"
 
 
 def main():
@@ -17,11 +18,49 @@ def main():
     client.send(IP.encode(ENCODING))
     while True:
         command = input("> ")
-        if (command == "CLOSE"):
-            client.send(command.encode(ENCODING))
-            os._exit(os.EX_OK)
+        command = command.split("$")
+
+        if len(command) == 1:
+            command = ''.join(command)
+        elif len(command) == 2:
+            command, file_name = command
+        else:
+            continue
+
+        if command == "CLOSE":
+            # Close server
+            print("Server is close")
+            break
+        elif command == "LOGOUT":
+            # Disconnect from server
+            print(f"{IP} disconnected")
+            break
+        elif command == "ADD":
+            # Public file in server
+            if file_name == ".":
+                data = ' '.join(os.listdir(DATA_PATH))
+            else:
+                flag = False
+                for file in os.listdir(DATA_PATH):
+                    if file == file_name:
+                        flag = True
+                        break
+                
+                if not flag:
+                    print("File is invalid")
+                    continue
+                else:
+                    data = file_name
+            
+            client.send(f"{command}${data}".encode(ENCODING))
+        elif command == "DELETE":
+            client.send(f"{command}${file_name}")
         else:
             print("Syntax error")
+        
+    client.send(command.encode(ENCODING))
+    os._exit(os.EX_OK)
+
         
 
 
